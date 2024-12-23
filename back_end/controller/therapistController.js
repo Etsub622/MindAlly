e42cd1f54b5e7e8f2f845b4adc5bdff38051962b
Rediet Haileab <wegenerediet@gmail.com>
2024-12-23 10:27:51 +0300

    login and signup

---
+++ back_end/controller/therapistController.js
@@ -0,0 +1,61 @@
+import bcrypt from "bcrypt";
+import { Therapist } from "../model/therapistModel.js";
+
+
+const registerTherapist = async (req, res) => {
+    try {
+
+        const { fullName, email, password, confirmPassword, specialization } = req.body
+        
+         if (password !== confirmPassword) {
+            return res.status(400).json({ message: "Passwords do not match" });
+        }
+    
+    const hashedPassword= await bcrypt.hash(password,10)
+    
+    const therapist = new Therapist({
+      FullName:fullName,
+      Email:email,
+      AreaofSpecification:specialization,
+      Password:hashedPassword
+    })
+    
+        await therapist.save()
+        res.status(200).json(therapist)
+        
+    } catch (error) {
+        console.log(error)
+        
+    }
+   
+    
+}
+
+const therapistLogin = async (req, res) => {
+    try {
+        const { Email, Password } = req.body;
+
+       
+        if (!Email || !Password) {
+            return res.status(400).json({ error: "Email and Password are required." });
+        }
+
+        const therapist = await Therapist.findOne({ Email });
+        if (!therapist) {
+            return res.status(404).json({ error: "Invalid email or password." });
+        }
+
+     
+        const isMatch = await bcrypt.compare(Password, therapist.Password);
+        if (!isMatch) {
+            return res.status(401).json({ error: "Invalid email or password." });
+        }
+
+        res.status(200).json({ message: "Login successful", Therapist});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "An error occurred during login." });
+    }
+};
+
+export {registerTherapist,therapistLogin}
---
+++ back_end/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import patientRoutes from "./routes/patientRoutes.js"
+import therapistRoutes from "./routes/therapistRoutes.js"
+ 
+
+
+dotenv.config();
+const app = express();
+app.use(bodyParser.json());
+
+
+mongoose
+    .connect(process.env.Mongo_url)
+    .then(() => {
+        console.log("Database connected succesfully")
+    })
+
+
+app.use(cors({
+ 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+  credentials:true
+}));
+
+app.use("/api/patinet",patientRoutes)
+app.use("/api/therapist", therapistRoutes)
+
+
+app.listen(process.env.PORT, () => {
+    console.log(   `server is running on port ${process.env.PORT}`)
+})
+
