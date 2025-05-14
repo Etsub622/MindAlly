e55c4874b64a9f70ed46753783fd754c66ab8276
meronabebe <meriabebehab@gmail.com>
2025-05-14 11:56:52 +0300

    update schedule

--- back_end/model/sessionModel.js
+++ back_end/model/sessionModel.js
@@ -5,7 +5,8 @@ const sessionSchema = new mongoose.Schema(
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     therapistId: { type: mongoose.Schema.Types.ObjectId, ref: "Therapist", required: true },
     date: { type: String, required: true }, // Format: YYYY-MM-DD
-    timeSlot: { type: String, required: true }, // Example: "10:00 AM"
+    startTime: { type: String, required: true }, 
+    endTime: { type: String, required: true },   
     status: {
       type: String,
       enum: ["Pending", "Confirmed", "Completed", "Cancelled"],
