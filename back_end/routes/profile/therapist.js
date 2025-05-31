e9649f8403d19b88bb7a106cb8c2eb2587b54ac0
halle32 <hannalegesse61@gmail.com>
2025-05-31 22:45:03 +0300

    added get unapproved therapists

--- back_end/routes/profile/therapist.js
+++ back_end/routes/profile/therapist.js
@@ -5,12 +5,14 @@ import {
   updateTherapist,
   deleteTherapist,
   getTopTherapists,
+  getUnapprovedTherapists,
 } from "../../controller/profile/therapistController.js";
 import { verifyToken } from "../../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", createTherapist);
+router.get("/unapproved", getUnapprovedTherapists);
 router.get("/:therapist_id", getTherapist);
 router.put("/:therapist_id", verifyToken, updateTherapist);
 router.delete("/:therapist_id", verifyToken, deleteTherapist);
